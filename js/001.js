3ca07be60a4068c69883466b7f35653b8ae8ab7c
Stef Bastiaansen <stef.bastiaansen@wijs.be>
2016-01-28 13:56:39 +0100

    Refactoring: Part 1 - Create object per experiment

---
+++ js/001.js
@@ -0,0 +1,78 @@
+"use strict";
+
+class Experiment001 extends Experiment{
+
+    constructor(context, canvas) {
+        super(context, canvas);
+        
+        this.particles = [];
+
+        // store mouse coordinates in a variable, but init in the middle of the window
+        this.mouse = {
+            x: canvas.width / 2,
+            y: canvas.width / 2
+        }
+    }
+
+    start() {
+        var ref = this; 
+
+        this.canvas.onmousemove = function(e) {
+        	ref.onmousemove(e);
+        }
+
+        this.interval = setInterval(function() {
+        	ref.showFrame()
+        }, 1000/100);
+    }
+
+    onmousemove (e) {
+        this.mouse.x = e.clientX,
+        this.mouse.y = e.clientY
+    }
+
+    showFrame() {
+        var remaining_particles = [];
+
+        // create a new particle
+        var p = {
+            x: this.mouse.x,
+            y: this.mouse.y,
+            speed: 1,
+            rotation: Math.random() * Math.PI * 2,
+            size: (20 * Math.random()) + 3,
+            color: fv.randomColor()
+        }
+
+        this.particles.push(p);
+
+        this.wipe();
+
+        // loop the existing particles and render them on the new location
+        for(var i = 0; i < this.particles.length; i++) {                 
+            this.c.fillRect(
+            	this.particles[i].x,
+            	this.particles[i].y,
+            	this.particles[i].size,
+            	this.particles[i].size
+        	);
+
+            this.c.fillStyle = this.particles[i].color;
+
+            this.particles[i].x += Math.sin(this.particles[i].rotation) * this.particles[i].speed;
+            this.particles[i].y += Math.cos(this.particles[i].rotation) * this.particles[i].speed; 
+            this.particles[i].size *= 0.99;
+
+            if (fv.inCanvas(this.particles[i])) {
+                remaining_particles.push(this.particles[i]);
+            }
+        }
+
+        this.particles = remaining_particles
+    }
+}
+
+var experiment = new Experiment001('2d', fv.canvas);
+
+experiment.start();
+
--- js/general.js
+++ js/general.js
@@ -1,19 +1,68 @@
-function inCanvas(p)
-{
-	return (
-		p.x > -100 &&
-		p.y > -100 &&
-		p.x < canvas.width + 100 &&
-		p.y < canvas.height + 100
-	);
+"use strict";
+
+class Experiment {
+	constructor(context, canvas) {
+		this.canvas = canvas;
+        this.c = this.canvas.getContext(context);
+        this.interval = null;
+        this.wipe();
+	}
+
+    pause() {
+
+    }
+
+    quit() {
+
+    }
+
+    wipe() {
+    	this.c.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    }
 }
 
-function randomColor()
-{
-	return '#' + (Math.random()*0xFFFFFF<<0).toString(16);
-}
 
-var canvas = document.createElement('canvas');
-document.body.appendChild(canvas);
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+var fv = {
+	canvas: null,
+
+	randomColor: function() {
+		return '#' + (Math.random()*0xFFFFFF<<0).toString(16);
+	},
+	
+	getCurrentNumber: function() {
+		var number = '001';
+
+		if (document.location.hash.length == 4) {
+			number = document.location.hash.substring(1);
+		}
+
+		return number;
+	},
+
+	createCanvasElement: function() {
+		fv.canvas = document.createElement('canvas');
+		document.body.appendChild(fv.canvas);
+		fv.canvas.width = window.innerWidth;
+		fv.canvas.height = window.innerHeight;	
+	},
+
+	inCanvas: function(p) {
+		return (
+			p.x > -100 &&
+			p.y > -100 &&
+			p.x < fv.canvas.width + 100 &&
+			p.y < fv.canvas.height + 100
+		);
+	},
+
+	loadFirstScript: function() {
+		var script = document.createElement('script');
+		
+		script.src = 'js/' + fv.getCurrentNumber() + '.js';
+		document.body.appendChild(script);
+	}
+};
+
+fv.createCanvasElement();
+fv.loadFirstScript();
+
